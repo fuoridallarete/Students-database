3ddde927995ec965c8c5b0852eb857b3c67747a1
sa cux <fuoridallarete@users.noreply.github.com>
2019-03-29 15:28:04 +0100

    Update Students.java
    methods to: enroll, pay tuition and view balance

--- Students.java
+++ Students.java
@@ -33,4 +33,36 @@ public class Student {
     this.studentId = gradeYear + "" + id;
     // adding "" makes it a string not an int
   }
+  //enroll in courses
+  public void enroll(){
+    //get inside a loop and user hits 0 to enroll
+    do {
+      System.out.print("Enter course to enroll (Q to quit): ");
+      Scanner in = new Scanner(System.in);
+      String course = in.nextLine();
+      if (!course.equals("Q")) {
+        courses = courses + "\n" + course;
+        tuitionBalance = tuitionBalance + costOfCourse;
+      }else {
+        break;
+      }
+    }while(1 != 0);
+
+    System.out.println("Enrolled in " + courses);
+  }
+  //view balance
+  public void viewBalance(){
+    System.out.println("Your balance is " + tuitionBalance);
+  }
+  //pay tuition
+  public void payTuition(){
+    viewBalance();
+    System.out.print("Enter your payment: $");
+    Scanner in = new Scanner(System.in);
+    int payment = in.nextInt();
+    tuitionBalance = tuitionBalance - payment;
+    System.out.println("Thank you for your payment of " + payment);
+    viewBalance();
+  }
+  //show student' status
  } 
