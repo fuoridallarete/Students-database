4169e397b4a35c4dce1cf737db95fc1627935a07
sa cux <fuoridallarete@users.noreply.github.com>
2019-03-29 15:25:16 +0100

    Create StudentsDBapp.java

---
+++ StudentsDBapp.java
@@ -0,0 +1,16 @@
+package studentsDB;
+
+import java.util.Scanner;
+
+public class StudentsDBapp {
+  public static void main(String[] args){
+    //ask how many users you want to add
+    System.out.print("Enter number of students to enroll: ");
+    Scanner in = new Scanner(System.in);
+    //create array of students (array of objects)
+    int numOfStudents = in.nextInt();
+    Student[] students = new Student[numOfStudents];
+ }
+} 
+ 
+ 
